7be8816af385ac9a6321906154d12fc9e1002bad
duc-minh-droid <ducminhcsp@gmail.com>
2024-10-26 15:45:05 +0100

    hello

---
+++ Player.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player implements Runnable {
+    private final int id;
+    private final List<Integer> hand = new ArrayList<>();
+    private final Deck leftDeck;
+    private final Deck rightDeck;
+    // private boolean active = true;
+
+    public Player(int id, Deck leftDeck, Deck rightDeck) {
+        this.id = id;
+        this.leftDeck = leftDeck;
+        this.rightDeck = rightDeck;
+    }
+}
