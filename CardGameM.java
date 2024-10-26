dd45f57464779317cae92c0b00132445146570f1
duc-minh-droid <ducminhcsp@gmail.com>
2024-10-26 16:41:32 +0100

    Update CardGameM.java

--- CardGameM.java
+++ CardGameM.java
@@ -6,10 +6,11 @@ public class CardGameM {
         List<Player> players = new ArrayList<>();
         List<Deck> decks = new ArrayList<>();
 
+        // Get user input 
         int n = 4;
-
         List<Integer> pack = HelperFunctions.readPack("four.txt", n);
         System.out.println(pack.size());
+
         // Init decks
         for (int i = 0; i < n; i++) {
             decks.add(new Deck(i + 1));
@@ -20,6 +21,7 @@ public class CardGameM {
             players.add(new Player(i, decks.get(i), decks.get(i==0?decks.size()-1:i-1)));
         }
 
+        // Distribute cards
         HelperFunctions.distributeCards(players, decks, pack);
     }
 }
